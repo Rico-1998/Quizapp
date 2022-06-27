91ecae5048c1d9991f7eeb06a9ef0ceda5e3c9de
Rico Denkewitz <rico@MBP-von-Rico.fritz.box>
2022-06-27 17:52:13 +0200

    third commit

--- script.js
+++ script.js
@@ -1,61 +1,12 @@
-let questions = [
-    {
-        "question": "Wer hat HTML erfunden?",
-        "answer_1": "Robbie Williams",
-        "answer_2": "Lady Gaga",
-        "answer_3": "Tim Berners-Lee",
-        "answer_4": "Justin Bieber",
-        "right_answer": 3,
-    },
 
-    {
-        "question": "Was bedeutet das HTML Tag &lt;a&gt;?",
-        "answer_1": "Text Fett",
-        "answer_2": "Container",
-        "answer_3": "Ein Link",
-        "answer_4": "Kursiv",
-        "right_answer": 3,
-    },
-
-    {
-        "question": "Wie bindet man eine Website in eine Website",
-        "answer_1": "&lt;iframe&gt;, &lt;frame&gt;, and &lt;frameset&gt",
-        "answer_2": "&lt;iframe&gt;",
-        "answer_3": "&lt;frame&gt;",
-        "answer_4": "&lt;frameset&gt",
-        "right_answer": 2,
-    },
-
-    {
-        "question": "Welches Attribut kann man NICHT für Textarea verwenden?",
-        "answer_1": "readonly",
-        "answer_2": "max",
-        "answer_3": "from",
-        "answer_4": "spellcheck",
-        "right_answer": 1,
-    },
-
-    {
-        "question": "Wie wählst du alle Elemente vom Typ &lt;a&gt; mit dem attribut title aus?",
-        "answer_1": "a[title]{...}",
-        "answer_2": "a > title {...}",
-        "answer_3": "a.title {...}",
-        "answer_4": "a=title {...}",
-        "right_answer": 1,
-    },
-
-    {
-        "question": "Wie definiert man in Javascript eine Variable?",
-        "answer_1": "let 100 = rate;",
-        "answer_2": "100 = let rate;",
-        "answer_3": "rate = 100;",
-        "answer_4": "let rate = 100;",
-        "right_answer": 4,
-    },
-];
 
 
 let currentQuestion = 0;
+let rightAnswers = 0;
+let AUDIO_SUCCESS = new Audio('https://cdn.freesound.org/previews/528/528957_10334845-lq.mp3');
+let AUDIO_FAIL = new Audio('https://cdn.freesound.org/previews/483/483598_6436863-lq.mp3');
+
+
 
 
 function init() {
@@ -66,6 +17,65 @@ function init() {
 
 function showQuestion() {
     let question = questions[currentQuestion];
+
+    if (gameIsOver()) {
+        showEndScreen();
+    } else {
+        updateProgressBar();
+        updateToNextQuestion(question);
+    }
+}
+
+
+function answer(selection) {
+    let question = questions[currentQuestion];
+    let selectedQuestionNumber = selection.slice(-1);
+    let idOfRightAnswer = `answer_${question['right_answer']}`;
+    // idOfRightAnswer bekommt als wert einen html code zugewiesen und zwar answer an der stelle question was null ist und die richtige antwort 
+
+    if (rightAnswerSelected(selectedQuestionNumber, question)) {
+        document.getElementById(selection).parentNode.classList.add('bg-success');
+        AUDIO_SUCCESS.play();
+        rightAnswers++;
+    } else {
+        document.getElementById(selection).parentNode.classList.add('bg-danger');
+        // weil als parameter im html code die id mitgegeben wurde kann man selection ansprechen und brauch nicht jede id einzeln ansprechen.
+        document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
+        AUDIO_FAIL.play();
+    }
+    document.getElementById('next-Button').disabled = false;
+}
+
+
+function nextQuestion() {
+    currentQuestion++;
+    document.getElementById('next-Button').disabled = true;
+    resetButtonAnswer();
+    showQuestion();
+}
+
+
+function resetButtonAnswer() {
+    document.getElementById('answer_1').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_1').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-success');
+}
+
+function replay() {
+    document.getElementById('endscreen').style = ('display: none;');
+    document.getElementById('question-body').style = ('');
+    currentQuestion = 0;
+    rightAnswers = 0;
+    init();
+}
+
+function updateToNextQuestion(question) {
+    document.getElementById('question-number').innerHTML = currentQuestion + 1;
     document.getElementById('questionText').innerHTML = question['question'];
     document.getElementById('answer_1').innerHTML = question['answer_1'];
     document.getElementById('answer_2').innerHTML = question['answer_2'];
@@ -74,12 +84,26 @@ function showQuestion() {
 }
 
 
-function answer(selection) {
-    let question = questions[currentQuestion];
-    let selectedQuestionNumber = selection.slice(-1);
-    if (selectedQuestionNumber == question['right_answer']) {
-        document.getElementById(selection).parentNode.classList.add('bg-success');
-    } else {
-        document.getElementById(selection).parentNode.classList.add('bg-danger');
-    }
+function showEndScreen() {
+    document.getElementById('endscreen').style = ``;
+    document.getElementById('question-body').style = 'display: none';
+    document.getElementById('all-questions').innerHTML = questions.length;
+    document.getElementById('right-answers').innerHTML = rightAnswers;
+}
+
+
+function gameIsOver() {
+    return currentQuestion >= questions.length;
+}
+
+
+function updateProgressBar() {
+    let percent = currentQuestion / questions.length; // die variable wird deklariert
+    percent = Math.round(percent * 100); // hier wird sie nicht ersetzt sondern ein neuer wert hinzugefügt
+    document.getElementById('progress').innerHTML = `${percent} %`;
+    document.getElementById('progress').style = `width: ${percent}%;`;
+}
+
+function rightAnswerSelected(selectedQuestionNumber, question) {
+    return selectedQuestionNumber == question['right_answer'];
 }
