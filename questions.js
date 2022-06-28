a7c147de9f851db7175566e859384908f08f4472
Rico Denkewitz <rico@MBP-von-Rico.fritz.box>
2022-06-28 13:32:57 +0200

    fourth  commit

--- questions.js
+++ questions.js
@@ -1,4 +1,4 @@
-let questions = [
+let html_questions = [
     {
         "question": "Wer hat HTML erfunden?",
         "answer_1": "Robbie Williams",
@@ -62,3 +62,135 @@ let questions = [
         "right_answer": 4,
     }
 ];
+
+
+let css_questions = [
+    {
+        "question": "Was bedeutet CSS?",
+        "answer_1": "Computer Style Sheet",
+        "answer_2": "Computer Sheet Style",
+        "answer_3": "Completely Style Sheet",
+        "answer_4": "Cascading Style Sheet",
+        "right_answer": 4,
+    },
+
+    {
+        "question": "Wann wurde CSS erfunden?",
+        "answer_1": "1992",
+        "answer_2": "1994",
+        "answer_3": "1996",
+        "answer_4": "1997",
+        "right_answer": 3,
+    },
+
+    {
+        "question": "Ein externes Stylesheet ist wofür gut?",
+        "answer_1": "eine einzelne Website",
+        "answer_2": "Mehrere Websiten",
+        "answer_3": "Garnichts von allem",
+        "answer_4": "Einige wenige Seiten",
+        "right_answer": 2,
+    },
+
+    {
+        "question": "Wo sollte die CSS Datei geschrieben werden?",
+        "answer_1": "Im Head Bereich",
+        "answer_2": "Im Footer Bereich",
+        "answer_3": "Im Body Bereich",
+        "answer_4": "Im Section Bereich",
+        "right_answer": 1,
+    },
+
+    {
+        "question": "Welches HTML Tag nimmt man für ein internes Style Sheet",
+        "answer_1": "&ltscript&gt",
+        "answer_2": "&ltcss&gt",
+        "answer_3": "&ltstyle&gt",
+        "answer_4": "&ltlink&gt",
+        "right_answer": 4,
+    },
+
+    {
+        "question": "Was ist die korrekte Syntax?",
+        "answer_1": "body:color=blue;",
+        "answer_2": "body{color:blue}",
+        "answer_3": "{body:color:blue}",
+        "answer_4": "{body;color:blue}",
+        "right_answer": 1,
+    },
+
+    {
+        "question": "Welches CSS Property nutzt man für die Hintergrundfarbe?",
+        "answer_1": "color",
+        "answer_2": "bg-color",
+        "answer_3": "background-color",
+        "answer_4": "color-bg",
+        "right_answer": 3,
+    },
+]
+
+
+let auto_questions = [
+    {
+        "question": "In welcher Stadt hat die Tuning Firma Brabus ihren Hauptsitz?",
+        "answer_1": "Zuffenhausen",
+        "answer_2": "Bottrop",
+        "answer_3": "Ingolstadt",
+        "answer_4": "München",
+        "right_answer": 2,
+    },
+
+    {
+        "question": "Wieviel Hubraum hat Ein Nissan GTR R35?",
+        "answer_1": "4 Liter",
+        "answer_2": "2,5 Liter",
+        "answer_3": "3Liter",
+        "answer_4": "3,8 Liter",
+        "right_answer": 4,
+    },
+
+    {
+        "question": "Wieviel Zylinder hat ein Bugatti Chiron",
+        "answer_1": "8",
+        "answer_2": "10",
+        "answer_3": "12",
+        "answer_4": "16",
+        "right_answer": 4,
+    },
+
+    {
+        "question": "Wieviel Ps hat ein Porsche 911 GT2 Rs?",
+        "answer_1": "800Ps",
+        "answer_2": "740Ps",
+        "answer_3": "700Ps",
+        "answer_4": "680Ps",
+        "right_answer": 3,
+    },
+
+    {
+        "question": "Wofür ist ein Blow Off Ventil?",
+        "answer_1": "Um Ladedruck aufzubauen",
+        "answer_2": "Um Ladedruck abzulassen",
+        "answer_3": "Abgase auslassen",
+        "answer_4": "Kurbelgehäuse entlüften",
+        "right_answer": 2,
+    },
+
+    {
+        "question": "Wieviel Top Speed fährt ein R35 GTR?",
+        "answer_1": "350 km/h",
+        "answer_2": "300 km/h",
+        "answer_3": "320km/h",
+        "answer_4": "315km/h",
+        "right_answer": 4,
+    },
+
+    {
+        "question": "Welches Auto wünsche Ich mir wohl?",
+        "answer_1": "BMW M6",
+        "answer_2": "Nissan GTR R35 aka (Godzilla)",
+        "answer_3": "Porsche Panamera",
+        "answer_4": " Audi RS7",
+        "right_answer": 2,
+    },
+]
--- script.js
+++ script.js
@@ -1,26 +1,46 @@
-
-
-
 let currentQuestion = 0;
 let rightAnswers = 0;
-let AUDIO_SUCCESS = new Audio('https://cdn.freesound.org/previews/528/528957_10334845-lq.mp3');
-let AUDIO_FAIL = new Audio('https://cdn.freesound.org/previews/483/483598_6436863-lq.mp3');
+let selectedQuiz = html_questions;
+let AUDIO_SUCCESS = new Audio('./audio/success.mp3');
+let AUDIO_FAIL = new Audio('./audio/fail.mp3');
 
 
 
 
 function init() {
-    document.getElementById('number').innerHTML = questions.length;
+    document.getElementById('number').innerHTML = selectedQuiz.length;
     showQuestion();
 }
 
 
+function changeQuiz(select) {
+    selectedQuiz = select;
+    replay();
+}
+
+
+function showHtml(select) {
+    changeQuiz(select);
+}
+
+
+function showCSS(select) {
+    changeQuiz(select);
+}
+
+
+function showAuto(select) {
+    changeQuiz(select);
+}
+
+
 function showQuestion() {
-    let question = questions[currentQuestion];
+    let question = selectedQuiz[currentQuestion];
 
     if (gameIsOver()) {
         showEndScreen();
     } else {
+        document.getElementById('overlay').style = 'display: none';
         updateProgressBar();
         updateToNextQuestion(question);
     }
@@ -28,7 +48,7 @@ function showQuestion() {
 
 
 function answer(selection) {
-    let question = questions[currentQuestion];
+    let question = selectedQuiz[currentQuestion];
     let selectedQuestionNumber = selection.slice(-1);
     let idOfRightAnswer = `answer_${question['right_answer']}`;
     // idOfRightAnswer bekommt als wert einen html code zugewiesen und zwar answer an der stelle question was null ist und die richtige antwort 
@@ -43,6 +63,7 @@ function answer(selection) {
         document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
         AUDIO_FAIL.play();
     }
+    document.getElementById('overlay').style = 'display: block';
     document.getElementById('next-Button').disabled = false;
 }
 
@@ -56,49 +77,58 @@ function nextQuestion() {
 
 
 function resetButtonAnswer() {
-    document.getElementById('answer_1').parentNode.classList.remove('bg-danger');
-    document.getElementById('answer_1').parentNode.classList.remove('bg-success');
-    document.getElementById('answer_2').parentNode.classList.remove('bg-danger');
-    document.getElementById('answer_2').parentNode.classList.remove('bg-success');
-    document.getElementById('answer_3').parentNode.classList.remove('bg-danger');
-    document.getElementById('answer_3').parentNode.classList.remove('bg-success');
-    document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
-    document.getElementById('answer_4').parentNode.classList.remove('bg-success');
+    resetBtn('answer_1');
+    resetBtn('answer_2');
+    resetBtn('answer_3');
+    resetBtn('answer_4');
+}
+
+function resetBtn(btnId) {
+    document.getElementById(btnId).parentNode.classList.remove('bg-danger');
+    document.getElementById(btnId).parentNode.classList.remove('bg-success');
 }
 
 function replay() {
     document.getElementById('endscreen').style = ('display: none;');
     document.getElementById('question-body').style = ('');
+    document.getElementById('overlay').style = 'display: none';
     currentQuestion = 0;
     rightAnswers = 0;
+    resetButtonAnswer();
     init();
 }
 
 function updateToNextQuestion(question) {
     document.getElementById('question-number').innerHTML = currentQuestion + 1;
     document.getElementById('questionText').innerHTML = question['question'];
-    document.getElementById('answer_1').innerHTML = question['answer_1'];
-    document.getElementById('answer_2').innerHTML = question['answer_2'];
-    document.getElementById('answer_3').innerHTML = question['answer_3'];
-    document.getElementById('answer_4').innerHTML = question['answer_4'];
+    update('answer_1', question);
+    update('answer_2', question);
+    update('answer_3', question);
+    update('answer_4', question);
 }
+
+
+function update(questionId, question) {
+    document.getElementById(questionId).innerHTML = question[questionId];
+}
+
 
 
 function showEndScreen() {
     document.getElementById('endscreen').style = ``;
     document.getElementById('question-body').style = 'display: none';
-    document.getElementById('all-questions').innerHTML = questions.length;
+    document.getElementById('all-questions').innerHTML = selectedQuiz.length;
     document.getElementById('right-answers').innerHTML = rightAnswers;
 }
 
 
 function gameIsOver() {
-    return currentQuestion >= questions.length;
+    return currentQuestion >= selectedQuiz.length;
 }
 
 
 function updateProgressBar() {
-    let percent = currentQuestion / questions.length; // die variable wird deklariert
+    let percent = (currentQuestion + 1) / selectedQuiz.length; // die variable wird deklariert
     percent = Math.round(percent * 100); // hier wird sie nicht ersetzt sondern ein neuer wert hinzugefügt
     document.getElementById('progress').innerHTML = `${percent} %`;
     document.getElementById('progress').style = `width: ${percent}%;`;
